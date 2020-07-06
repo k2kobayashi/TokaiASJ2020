7ff102d6f20d15419036c0ca38b1da70c149c0fd
Kazuhiro KOBAYASHI <root.4mac@gmail.com>
2020-07-06 18:01:09 +0900

    add files

---
+++ parse_csv.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright (c) 2020 Kazuhiro KOBAYASHI <root.4mac@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+
+csvf = Path("./sheet1.csv")
+if not csvf.exists():
+    raise ValueError("Check csv file")
+
+df = pd.read_csv(csvf)
+
+
+print("### 10:00-10:53 概要公演　前半 (概要講演:Zoom開催) [[ZoomURL]](https://hoge.com)")
+for idx, row in df.iterrows():
+    if not np.isnan(row["発表番号"]):
+        print("- **発表番号{}** {}-{}".format(int(row["発表番号"]), row["開始時間"], row["終了時間"]))
+        print("\t- {} （{} {}）".format(row["氏名"], row["大学"], row["研究室"]))
+        print("\t- {}  [(資料)]({})".format(row["タイトル"], row["資料"]))
+    else:
+        print("### 11:00-12:00 概要公演　後半 (概要講演:Zoom開催) [(ZoomURL)](https://hoge.com)")
---
+++ run.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+#
+# run.sh
+# Copyright (C) 2020 Kazuhiro KOBAYASHI <root.4mac@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+# download csv from google spreadsheets
+curl -L -o sheet1.csv \
+    'https://docs.google.com/spreadsheets/d/1epd7pzJdWIFO0hKivWZOpIbLSfUHGzdCAoiIueGRApc/export?format=csv'
+
+# parse spreadsheets
+python parse_csv.py > middle.md
+
+# concate as markdown
+cat top.md middle.md bottom.md > README.md
